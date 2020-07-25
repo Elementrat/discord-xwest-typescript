6de5be28d5f3c1b1d77e4c70be92958ed5b57d05
Alex Sohail <elementrat@gmail.com>
2020-07-24 22:09:15 -0700

    make less verbose

--- src/util.ts
+++ src/util.ts
@@ -2,43 +2,28 @@
 import { Config } from './config';
 
 export function shuffle(a: Array<any>): Array<any> {
-    let j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-    return a;
+  let j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
 }
 
 function replaceAll(string, search, replace) {
-    return string.split(search).join(replace);
+  return string.split(search).join(replace);
 }
 
 export function SanitizeMarkdown(str: string): string {
-    return replaceAll(replaceAll(replaceAll(str, '`', ''), '\\', ''), `\n`, '');
+  return replaceAll(replaceAll(replaceAll(str, '`', ''), '\\', ''), `\n`, '');
 }
 
-
 export function checkIfMod(id: string): boolean {
-    let isMod = false;
-    Config.moderatorIds.map(val => {
-        if (val === id) {
-            isMod = true;
-        }
-    })
-
-    return isMod;
+  return Config.moderatorIds.some((moderatorId) => moderatorId === id);
 }
 
 export function checkIfDuplicate(array: any[], id: string): boolean {
-    let found = false;
-    array.map(val => {
-        if (val == id) {
-            found = true;
-        }
-    });
-
-    return found;
+  return array.some((arrayElement) => arrayElement == id);
 }
